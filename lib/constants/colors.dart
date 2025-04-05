54dc9bd910c15cabe2c7f91c662fd1bb33b03490
SoYeon Park_frontend.git <psy46500@naver.com>
2025-04-05 20:31:35 +0900

    feat: 컬러 등록

---
+++ lib/constants/colors.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // 🔷 GrayScale
+  static const Color gray950 = Color(0xFF0E1117);
+  static const Color gray900 = Color(0xFF141B23);
+  static const Color gray800 = Color(0xFF1E2228);
+  static const Color gray700 = Color(0xFF38414C);
+  static const Color gray600 = Color(0xFF4A5260);
+  static const Color gray500 = Color(0xFF5F6775);
+  static const Color gray400 = Color(0xFF77828F);
+  static const Color gray300 = Color(0xFF909CA8);
+  static const Color gray200 = Color(0xFFB7C1C9);
+  static const Color gray100 = Color(0xFFD9E0E7);
+  static const Color gray50  = Color(0xFFE9F1F6);
+  static const Color gray20  = Color(0xFFFCFCFC);
+
+  // 🔳 Transparency (gray with opacity)
+  static const Color trans900 = Color(0xE60E1117); // 90%
+  static const Color trans800 = Color(0xCC0E1117); // 80%
+  static const Color trans700 = Color(0xB30E1117); // 70%
+  static const Color trans600 = Color(0x990E1117); // 60%
+  static const Color trans500 = Color(0x800E1117); // 50%
+  static const Color tranS400 = Color(0x660E1117); // 40%
+  static const Color trans300 = Color(0x4D0E1117); // 30%
+  static const Color trans200 = Color(0x330E1117); // 20%
+  static const Color trans100 = Color(0x1A0E1117); // 10%
+  static const Color trans50  = Color(0x0D0E1117); // 5%
+
+  // 🔵 Primary
+  static const Color pri900 = Color(0xFF142539);
+  static const Color pri800 = Color(0xFF1F2B3A);
+  static const Color pri700 = Color(0xFF263545);
+  static const Color pri600 = Color(0xFF32455B);
+  static const Color pri500 = Color(0xFF36526F);
+  static const Color pri400 = Color(0xFF3A5F83);
+  static const Color pri300 = Color(0xFF3C648B);
+  static const Color pri200 = Color(0xFF507B9F);
+  static const Color pri100 = Color(0xFF7290AE);
+  static const Color pri50  = Color(0xFF97B5D3);
+
+  // 🟢 Secondary
+  static const Color sec900 = Color(0xFF316A60);
+  static const Color sec800 = Color(0xFF31907F);
+  static const Color sec700 = Color(0xFF1FAE94);
+  static const Color sec600 = Color(0xFF00D1AA);
+  static const Color sec500 = Color(0xFF00E8BD);
+  static const Color sec400 = Color(0xFF00FFD0);
+  static const Color sec300 = Color(0xFF6FFFE5);
+  static const Color sec200 = Color(0xFF9EFFED);
+  static const Color sec100 = Color(0xFFCEFCF3);
+  static const Color sec50  = Color(0xFFEAFDFA);
+
+  // 🟥 Error
+  static const Color error = Color(0xFFF74353);
+}

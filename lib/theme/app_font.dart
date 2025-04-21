91fc0107f5dcdb05ed35fbc97c49a2228bd064e4
kim-yebeen <kimyebeen125@gmail.com>
2025-04-21 14:41:40 +0900

    폰트 등록

---
+++ lib/theme/app_font.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+
+class AppFonts {
+  static const String suit = 'SUIT';
+
+  // Head
+  static const h1_b = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w700,
+    fontSize: 28,
+    height: 1.0,
+    letterSpacing: -0.84,
+  );
+
+  static const h3_eb = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w800,
+    fontSize: 24,
+    height: 1.0,
+    letterSpacing: -0.72,
+  );
+
+  static const h3_sb = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+    height: 1.0,
+    letterSpacing: -0.72,
+  );
+
+  static const h4_b = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w700,
+    fontSize: 22,
+    height: 1.0,
+    letterSpacing: -0.66,
+  );
+
+  static const h5_b = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+    height: 1.0,
+    letterSpacing: -0.6,
+  );
+
+  static const h5_sb = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    height: 1.0,
+    letterSpacing: -0.6,
+  );
+
+  // Body
+  static const b1_b = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w700,
+    fontSize: 18,
+    height: 1.0,
+    letterSpacing: -0.54,
+  );
+
+  static const b1_sb = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 1.0,
+    letterSpacing: -0.54,
+  );
+
+  static const b2_b = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 1.0,
+    letterSpacing: -0.48,
+  );
+
+  static const b2_m = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    height: 1.0,
+    letterSpacing: -0.48,
+  );
+
+  static const b2_m_long = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    height: 1.6,
+    letterSpacing: -0.48,
+  );
+
+  static const b3_sb = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    height: 1.0,
+    letterSpacing: -0.42,
+  );
+
+  static const b3_sb_long = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    height: 1.6,
+    letterSpacing: -0.42,
+  );
+
+  static const b3_m = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 1.0,
+    letterSpacing: -0.42,
+  );
+
+  static const b3_r = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 1.0,
+    letterSpacing: -0.42,
+  );
+
+
+  // Caption
+  static const c1_b = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+    height: 1.0,
+    letterSpacing: -0.36,
+  );
+
+  static const c1_2 = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    height: 1.0,
+    letterSpacing: -0.36,
+  );
+
+  static const c2_b = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w700,
+    fontSize: 10,
+    height: 1.0,
+    letterSpacing: -0.3,
+  );
+
+  static const c2_sb = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w600,
+    fontSize: 10,
+    height: 1.0,
+    letterSpacing: -0.3,
+  );
+
+  static const c3_sb = TextStyle(
+    fontFamily: suit,
+    fontWeight: FontWeight.w600,
+    fontSize: 8,
+    height: 1.0,
+    letterSpacing: -0.3,
+  );
+}

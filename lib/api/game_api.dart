cc178faa36acb498f5ab67f1d85edc183c6ea7b5
SeoHyeRyeong <94300234+SeoHyeRyeong@users.noreply.github.com>
2025-05-11 11:51:57 +0900

    game API URL을 .env에서 불러오도록 변경

--- lib/api/game_api.dart
+++ lib/api/game_api.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+/*import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/game_response.dart';
 
@@ -29,4 +29,40 @@ class GameApi {
       throw Exception('게임 검색 실패: ${response.statusCode}');
     }
   }
+}*/
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../models/game_response.dart';
+
+class GameApi {
+  static Future<GameResponse> searchGame({
+    required String awayTeam,
+    required String date,
+    required String time,
+  }) async {
+    final backendUrl = dotenv.env['BACKEND_URL'];
+    if (backendUrl == null) throw Exception('백엔드 URL이 설정되지 않았습니다');
+
+    final Uri url = Uri.parse('$backendUrl/games/search').replace(
+      queryParameters: {
+        'awayTeam': awayTeam,
+        'date': date,
+        'time': time,
+      },
+    );
+
+    final response = await http.get(
+      url,
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final dynamic decoded = jsonDecode(utf8.decode(response.bodyBytes));
+      return GameResponse.fromJson(decoded);
+    } else {
+      throw Exception('게임 검색 실패: ${response.statusCode}');
+    }
+  }
 }
+

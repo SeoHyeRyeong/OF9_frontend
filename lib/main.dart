bf2addfb6bc911edcd93c92bce85d8c00e0cc8a3
SoYeon Park_frontend.git <psy46500@naver.com>
2025-12-14 16:57:26 +0900

    refactor: 스플래시, main 최적화

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-import 'package:frontend/features/onboarding_login/kakao_auth_service.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:frontend/features/onboarding_login/splash_screen.dart';
@@ -13,14 +12,8 @@ import 'package:clarity_flutter/clarity_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
-  KakaoSdk.init(nativeAppKey: dotenv.env['NATIVE_APP_KEY']);
 
-  await SystemChrome.setEnabledSystemUIMode(
-    SystemUiMode.edgeToEdge,
-  );
-
-  // SystemUiOverlayStyle 설정
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
@@ -32,45 +25,57 @@ Future<void> main() async {
       systemNavigationBarContrastEnforced: false,
     ),
   );
-
-  await SystemChrome.setPreferredOrientations([
+  SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-  final kakaoAuthService = KakaoAuthService();
-  final isLoggedIn = await kakaoAuthService.hasStoredTokens();
+  runApp(const MyApp());
 
-  print('🚀 앱 시작 - 로그인 상태: $isLoggedIn');
+  _initializeInBackground();
+}
 
-  // Clarity 설정 추가
-  final clarityConfig = ClarityConfig(
-    projectId: dotenv.env['CLARITY_PROJECT_ID']!,
-    logLevel: LogLevel.None,
-  );
-
-  runApp(
-    ClarityWidget(
-      clarityConfig: clarityConfig,
-      app: ScreenUtilInit(
-        designSize: Size(360, 800),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) => MyApp(isLoggedIn: isLoggedIn),
-      ),
-    ),
-  );
+Future<void> _initializeInBackground() async {
+  await dotenv.load();
+  KakaoSdk.init(nativeAppKey: dotenv.env['NATIVE_APP_KEY']);
 }
 
 class MyApp extends StatelessWidget {
-  final bool isLoggedIn;
-  const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => RecordState(),
-      child: MaterialApp(
+      child: FutureBuilder(
+        future: _getClarityConfig(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ClarityWidget(
+              clarityConfig: snapshot.data as ClarityConfig,
+              app: _buildApp(),
+            );
+          }
+          return _buildApp();
+        },
+      ),
+    );
+  }
+
+  Future<ClarityConfig> _getClarityConfig() async {
+    await dotenv.load();
+    return ClarityConfig(
+      projectId: dotenv.env['CLARITY_PROJECT_ID']!,
+      logLevel: LogLevel.None,
+    );
+  }
+
+  Widget _buildApp() {
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
         title: 'Flutter Kakao Login',
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -85,7 +90,7 @@ class MyApp extends StatelessWidget {
           final mediaQueryData = MediaQuery.of(context);
           return MediaQuery(
             data: mediaQueryData.copyWith(
-              textScaler: TextScaler.linear(1.0),
+              textScaler: const TextScaler.linear(1.0),
             ),
             child: child!,
           );
@@ -99,7 +104,7 @@ class MyApp extends StatelessWidget {
           Locale('ko', 'KR'),
         ],
         locale: const Locale('ko', 'KR'),
-        home: SplashScreen(isLoggedIn: isLoggedIn),
+        home: const SplashScreen(),
       ),
     );
   }
